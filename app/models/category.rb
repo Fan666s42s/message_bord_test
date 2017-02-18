fd4f03666046aecec1acdbfe55dd708f6b698bc5
Fan666s42s <x666s42@gmail.com>
2017-02-18 17:14:03 +0800

    20170218 new post

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :connections  
   has_many :posts, through: :connections
+
+
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,6 @@
 class Connection < ApplicationRecord
   belongs_to :post
   belongs_to :category
+
+  serialize :interests
 end
